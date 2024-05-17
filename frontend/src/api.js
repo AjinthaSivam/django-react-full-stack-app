338f85b83ee822b61d4c6910379cbfb1d360babd
AjinthaSivam <ajinthasivam2019@gmail.com>
2024-05-17 09:35:02 +0530

    update api in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,13 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiURL = "https://97cda304-9537-4bbd-a3e3-059414b22bea-dev.e1-us-east-azure.choreoapis.dev/djangoreactfullstackapp/backend/rest-api-be2/v1"
+const apiURL = "https://97cda304-9537-4bbd-a3e3-059414b22bea-dev.e1-us-east-azure.choreoapis.dev/djangoreactfullstackapp/backend/rest-api-be2/v1";
+
+const baseURL = import.meta.env.VITE_API_URL || apiURL;
 
 const api = axios.create({
-	baseURL: `${import.meta.env.VITE_API_URL}/` ? `${import.meta.env.VITE_API_URL}/` : apiURL
-})
+	baseURL: baseURL
+});
 
 api.interceptors.request.use(
 	(config) => {
